252c1fd1f4061a00fead517f8f335b255399cac2
rorode8 <rorode8@hotmail.com>
2021-05-06 17:45:49 -0500

    debugging mail

--- pagina-web/__init__.py
+++ pagina-web/__init__.py
@@ -6,7 +6,6 @@ import requests
 import smtplib, ssl
 import pymongo
 import os
-import pgeocode
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from pyfiscal.generate import GenerateRFC, GenerateCURP, GenerateNSS, GenericGeneration
@@ -40,11 +39,123 @@ def hello_world():
 
 	return render_template('index.html', request_method=request_method)
 
-def sendMail(nombre,folio,email):
+@app.route('/confirm/<string:curp>', methods=['GET', 'POST'])
+def confirm(curp):
+	request_method = request.method
+	if request.method == 'POST':
+		print('------------------------------------------')
+		print(request.form)
+		nombre =	request.form['nombre']
+		apellido = request.form['apellido']
+		mail = request.form['mail']
+		zipc  = request.form['zip']
+		#check if curp is registered again
+		match2 = users.find_one({"id":curp});
+		if(match2):
+			return "CURP registrado"
+		else:
+			match = users.find_one({}, sort=[("folio", pymongo.DESCENDING)])
+
+			f = int(match['folio'])
+			f +=1
+			doc = {'id':curp,'nombre':nombre, 'apellido':apellido, 
+				'mail':mail,'zip_code':zipc, 'folio':f}
+
+			users.insert_one(doc)
+			sendMail(nombre, f)
+			return render_template('confirm.html', postal = zipc ,fol=f)
+
+	else:
+		return 'error'
+	
+	return 'cock'
+
+@app.route('/mapa/<string:zipcode>')
+def mapa(zipc):
+	try:
+		postal = int(zipc)
+		#mandar coordinadas al mapa por aqui
+		return render_template('mapa.html',zip_code=postal)
+	except:
+		return 'error'
+
+	return zipc
+
+@app.route('/registrar/<string:curp>', methods=['GET', 'POST'])
+def registrar(curp):
+
+	request_method = request.method
+
+	match2 = users.find_one({"id":curp});
+
+	#no es un curp
+	if(len(curp)!=18):
+		return 'CURP invalido';
+
+	print(match2)
+
+
+	#el curp ya esta
+	if(match2):
+		return "CURP registrado"
+
+	#cargar pagina para rellenar el resto de los datos
+	#if request.method == 'POST':
+		#render_template('register.html', request_method=request_method)
+
+	
+	return render_template('register.html',clav=curp, request_method=request_method)
+
+
+
+@app.route('/datos', methods=['GET', 'POST'])
+def datos():
+	request_method = request.method
+	if request.method == 'POST':
+		print(request.form)
+		s=request.form['fecha']
+		fech = s[8:]+'-'+s[5:7]+'-'+s[0:4]
+		kwargs = {
+		"complete_name": request.form['nombre'],
+		"last_name": request.form['apellidopat'],
+		"mother_last_name": request.form['apellidomat'],
+		"birth_date": fech,
+		"gender": request.form['gender'],
+		"city": request.form['estado'],
+		"state_code": "11111"
+		}
+		#do try catch
+		curpaprox = GenerateCURP(**kwargs)
+		curpgen = curpaprox.data
+		print(curpgen)
+		return redirect(url_for('registrar',curp=c))
+
+	return render_template('sinCURP.html')
+
+@app.route('/varios', methods=['GET', 'POST'])
+def varios():
+	request_method = request.method
+	if request.method == 'POST':
+		print('not cringe')
+		print(request.form)
+		
+		curps = request.form['texts'].split(',')
+		for c in curps:
+			if(len(c)!=18):
+				flash('Al menos un CURP es incorrecto')
+				return render_template('varios.html', request_method=request_method)
+		#upload to mongodb here
+		#for c in curps:
+
+
+	return render_template('varios.html', request_method=request_method)
+
+
+def sendMail(nombre,folio):
     port = 465  # For SSL
     mail = 'aadsi6449@gmail.com'
     password = 'Adsi123Adsi'
-    receiver = email
+    receiver = 'josepe430@gmail.com'
 
     message =  MIMEMultipart("alternative")
     message["Subject"] = "Confirmación registro MiVacuna"
@@ -77,152 +188,9 @@ def sendMail(nombre,folio,email):
         server.login(mail, password)
         server.sendmail(mail, receiver, message.as_string())
 
-@app.route('/confirm/<string:curp>', methods=['GET', 'POST'])
-def confirm(curp):
-	request_method = request.method
-	if request.method == 'POST':
-		print('------------------------------------------')
-		print(request.form)
-		nombre =	request.form['nombre']
-		apellido = request.form['apellido']
-		mail = request.form['mail']
-		zipc  = request.form['zip']
-		#check if curp is registered again
-		match2 = users.find_one({"id":curp});
-		if(match2):
-			return render_template('error.html')
-		else:
-			match = users.find_one({}, sort=[("folio", pymongo.DESCENDING)])
-
-			f = int(match['folio'])
-			f +=1
-			doc = {'id':curp,'nombre':nombre, 'apellido':apellido, 
-				'mail':mail,'zip_code':zipc, 'folio':f}
-
-			try:
-				users.insert_one(doc)
-			except:
-				return 'cringe 1'
-
-			#try:	
-				#sendMail(str(nombre), str(f), str(mail))
-			#except:
-				#return 'cringe 2'
-			return render_template('confirm.html', postal = zipc ,fol=f)
-
-	else:
-		return 'error'
-	
-	return 'error'
-
-@app.route('/mapa/<string:zipcode>')
-def mapa(zipcode):
-	#try:
-		postal = int(zipcode)
-		llave = data["google"]
-		#mandar coordinadas al mapa por aqui
-		nomi = pgeocode.Nominatim('mx')
-		df = nomi.query_postal_code(str(postal))
-		return render_template('mapa.html',lat = df.latitude, lng=df.longitude, llave = llave)
-	#except:
-		return 'error'
-
-		return zipcode
-
-@app.route('/registrar/<string:curp>', methods=['GET', 'POST'])
-def registrar(curp):
-
-	request_method = request.method
-
-	match2 = users.find_one({"id":curp});
-
-	#no es un curp
-	if(len(curp)!=18):
-		return 'CURP invalido';
-
-	print(match2)
-
-
-	#el curp ya esta
-	if(match2):
-		return render_template('error.html')
-
-	#cargar pagina para rellenar el resto de los datos
-	#if request.method == 'POST':
-		#render_template('register.html', request_method=request_method)
-
-	
-	return render_template('register.html',clav=curp, request_method=request_method)
-
-
-
-@app.route('/datos', methods=['GET', 'POST'])
-def datos():
-	request_method = request.method
-	if request.method == 'POST':
-		print(request.form)
-		s=request.form['fecha']
-		fech = s[8:]+'-'+s[5:7]+'-'+s[0:4]
-		kwargs = {
-		"complete_name": request.form['nombre'],
-		"last_name": request.form['apellidopat'],
-		"mother_last_name": request.form['apellidomat'],
-		"birth_date": fech,
-		"gender": request.form['gender'],
-		"city": request.form['estado'],
-		"state_code": "11111"
-		}
-		try:
-			curpaprox = GenerateCURP(**kwargs)
-			curpgen = curpaprox.data
-			print(curpgen)
-			return redirect(url_for('registrar',curp=curpgen))
-		except:
-			flash("al menos un dato no es valido")
-			render_template('sinCURP.html')
-
-	return render_template('sinCURP.html')
-
-@app.route('/varios', methods=['GET', 'POST'])
-def varios():
-	request_method = request.method
-	if request.method == 'POST':
-		print('not cringe')
-		print(request.form)
-		
-		curps = request.form['texts'].split(',')
-		for c in curps:
-			if(len(c)!=18):
-				flash('Al menos un CURP es incorrecto')
-				return render_template('varios.html', request_method=request_method)
-
-			match2 = users.find_one({"id":c});
-			if(match2):
-				flash('Al menos un CURP ha sido registrado previamente')
-				return render_template('varios.html', request_method=request_method)
-
-
-		#upload to mongodb here
-
-		#get folio
-		match = users.find_one({}, sort=[("folio", pymongo.DESCENDING)])
-
-		f = int(match['folio'])
-		
-		for c in curps:
-			f+=1
-			doc = {'id':c,'nombre':str(request.form['nombre']), 'apellido':str(request.form['apellido']), 
-			'mail':str(request.form['mail']),'zip_code':str(request.form['zipcode']), 'folio':f}
-
-			users.insert_one(doc)
-
-		return render_template('confirm.html', postal = str(request.form['zipcode']) ,fol=f)
-			
-
-
-	return render_template('varios.html', request_method=request_method)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
